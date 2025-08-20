9b5c59574b67536e9425f57403db5f047f552036
Diniz018 <italo.dnz01@gmail.com>
2025-08-20 12:26:57 -0300

    ok

---
+++ Calculadora/Calculadora.java
@@ -0,0 +1,11 @@
+package Calculadora;
+
+public class Calculadora {
+    
+    public Double numeroA;
+    public Double numeroB;
+
+    public void somar(){
+        System.out.printf("O resultaod = f.2% %n", numeroA + numeroB);
+    }
+}
---
+++ ExecutaCalculadora.java
@@ -0,0 +1,12 @@
+import Calculadora.Calculadora;
+
+public class ExecutaCalculadora {
+    
+    public static void main(String[] args) {
+        
+        Calculadora calc1 = new Calculadora();
+        Calculadora calc2 = new Calculadora();
+
+    }
+
+}
